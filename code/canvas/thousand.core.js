afc75f45700624b05189c3fe34ca3652b208e8f4
Paulfox <ppauloa37@gmail.com>
2025-06-25 13:09:19 -0300

    Create thousand.core.js

---
+++ code/canvas/thousand.core.js
@@ -0,0 +1,8 @@
+var request_files = function() {
+  var scriptr1 = document.createElement("script")
+  scriptr1.src = "/extern/thousand.vars.js"
+  var scriptr2 = document.createElement("script")
+  scriptr2.src = "/extern/thousand.modules.vars.js"
+  var divthousand = document.createElement("div")
+  divthousand.className = "thousand-files";
+}
