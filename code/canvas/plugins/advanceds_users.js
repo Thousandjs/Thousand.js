cd6933c350d81815140a0e75ddbe0746c47f1583
Paulfox <ppauloa37@gmail.com>
2025-06-25 17:41:28 -0300

    Create advanceds_users.js

---
+++ code/canvas/plugins/advanceds_users.js
@@ -0,0 +1,8 @@
+/*Credits*/
+const AUTHOR = "thousand ©"
+const VERSION = "0.1.8"
+
+/*VAR thousand plugin */
+const CANVAS = window.th.canvas
+const L_WIDTH = window.th.canvas.width
+const L_HEIGHT = window.th.canvas.height
