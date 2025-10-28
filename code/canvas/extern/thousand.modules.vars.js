4638bce6cc0b7890321d9ab4332704ba65ebb747
Paulfox <ppauloa37@gmail.com>
2025-10-28 19:48:15 -0300

    Delete code directory

--- code/canvas/extern/thousand.modules.vars.js
+++
@@ -1,7 +0,0 @@
-import * as THVARS from "thousand.vars.js"
-
-var viewvport_versions = function() {
-  THVARS.TD_V = null;
-  THVARS.DD_V = "uoltime"
-}
-viewport_versions();
--- code/canvas/plugins/advanceds_users.js
+++
@@ -1,8 +0,0 @@
-/*Credits*/
-const AUTHOR = "thousand ©"
-const VERSION = "0.1.8"
-
-/*VAR thousand plugin */
-const CANVAS = window.th.canvas
-const L_WIDTH = window.th.canvas.width
-const L_HEIGHT = window.th.canvas.height
